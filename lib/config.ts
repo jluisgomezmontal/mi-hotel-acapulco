928c7be86646037c83ba542eabf4c0c2871688a7
KIKE <you@example.com>
2025-11-04 14:13:01 -0600

    back added

--- lib/config.ts
+++ lib/config.ts
@@ -1,4 +1,4 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3009';
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || 'https://hotel-api-sdtg.onrender.com';
 
 export const API_ENDPOINTS = {
   rooms: `${API_URL}/api/rooms`,
